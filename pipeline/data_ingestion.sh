3d4342121aebef3bdcc3243f61cedb54ae0b6bba
Avinash Pasupulate <avinash.pasupulate@gmail.com>
2019-05-18 13:57:28 +0530

    corrected wd

--- pipeline/data_ingestion.sh
+++ pipeline/data_ingestion.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-DATA_ING=/src/preparation
-python3 $DATA_ING/data_ingestion.py $DATA_ING/config_prep.yaml $DATA_ING/load_query_gen.sql
+DATA_ING=src/preparation
+python3 $DATA_ING/data_ingestion_cli.py $DATA_ING/config_prep.yaml $DATA_ING/create_query_gen.sql $DATA_ING/load_bash_gen.sh
