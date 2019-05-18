8b8aecd3b851ee9b1827b5b88868273aacd45146
Avinash Pasupulate <avinash.pasupulate@gmail.com>
2019-05-18 13:51:55 +0530

    changed wd

--- pipeline/data_ingestion.sh
+++ pipeline/data_ingestion.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-DATA_ING=/Users/Avinash/Documents/datascience/dataops/src/preparation
+DATA_ING=/src/preparation
 python3 $DATA_ING/data_ingestion.py $DATA_ING/config_prep.yaml $DATA_ING/load_query_gen.sql
