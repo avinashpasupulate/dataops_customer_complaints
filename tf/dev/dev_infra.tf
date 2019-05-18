e40825412dd2fed6d6876100776d0d762e5afa16
Avinash Pasupulate <avinash.pasupulate@gmail.com>
2019-05-18 18:13:35 +0530

    changed tf provider profile

--- tf/dev/dev_infra.tf
+++ tf/dev/dev_infra.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
+    region = "us-east-1"
+    shared_credentials_file = "/.aws/credentials"
+    profile = "opencms"
 }
 
 #resource "aws_instance" "model" {
