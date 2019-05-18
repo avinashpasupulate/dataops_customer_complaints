067aab93fe9be3d15156531606c988c84e161f16
Avinash Pasupulate <avinash.pasupulate@gmail.com>
2019-05-18 14:19:58 +0530

    setup - corrected path

--- pipeline/create_infra.sh
+++ pipeline/create_infra.sh
@@ -1,6 +1,6 @@
 # #!/usr/bin/env bash
 TF_PATH=tf/dev
-TF_CRED=~/terraform.tfvars
+TF_CRED=/terraform.tfvars
 (cd $TF_PATH && terraform init)
 (cd $TF_PATH && terraform apply -var-file=$TF_CRED -auto-approve)
 
--- pipeline/destroy_infra.sh
+++ pipeline/destroy_infra.sh
@@ -1,4 +1,4 @@
 # #!/usr/bin/env bash
 TF_PATH=tf/dev
-TF_CRED=~/terraform.tfvars
+TF_CRED=/terraform.tfvars
 (cd $TF_PATH && terraform destroy -var-file=$TF_CRED -auto-approve)
